3d710a300fb5e8395b00b1df242d453759611863
Aslan <shamkhanislam00@gmail.com>
2021-06-07 17:19:33 +0500

    Initial Commit

---
+++ MyLocations/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyLocations
+//
+//  Created by Aslan Murat on 07.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
